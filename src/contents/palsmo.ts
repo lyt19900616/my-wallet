f0f4abd08146f0f8be55a11e5bed82ef75d1e14b
李耀亭 <liyaoting0616@126.com>
2025-08-12 15:49:33 +0800

    2

--- src/contents/palsmo.ts
+++ src/contents/palsmo.ts
@@ -1,40 +1,18 @@
 import type { PlasmoCSConfig } from 'plasmo';
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://*/*", "http://*/*"]
+  matches: ["https://*/*", "http://*/*", "http://localhost:*/*", "http://localhost:3000/"]
 }
 console.log('palsmo content script loaded');
 
 const injectWallectProvider = () => {
+  console.log('injecting wallet provider');
   const script = document.createElement('script')
-  script.type = 'text/javascript'
-  script.textContent = `
-    window.myWallet = {
-      // 请求账户 
-      requestAccounts: async () => {
-        return new Promise((resolve) => {
-          // 向background 发送消息
-          chrome.runtime.sendMessage(
-            { type: 'REQUEST_ACCOUNTS' }, 
-            (response) => resolve(response.accounts)
-          )
-        })
-      },
-      // 签名交易
-      signTransaction: async (tx) => {
-        return new Promise((resolve) => {
-          chrome.runtime.sendMessage(
-            { type: 'SIGN_TRANSACTION', data: tx },
-            (response) => resolve(response.signedTx)
-          )
-        })
-      }
-    }
-    // 触发DApp监听的事件
-    const event = new Event('walletconnect')
-    window.dispatchEvent(event)
-  `
-  document.head.appendChild(script)
+  script.src = chrome.runtime.getURL('injected-script.js')
+  console.log("src: ", script.src);
+  // script.onload = () => {
+  //   script.remove()
+  // }
 }
 
 window.addEventListener('load', () => {
